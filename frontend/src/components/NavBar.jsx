27d6fc76e108828db6337ac49ed9ba8f3e2dae17
David Kerr <kerrky@gmail.com>
2024-11-02 16:26:20 -0400

    Finished up styling details

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,19 +4,25 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <nav>
-      <div className="flex flex-row justify-between border-b-2 border-solid border-black py-4 px-2 align-middle bg-cyan-300">
+      <div
+        className="flex flex-row justify-between border-b-2 border-solid border-black py-4 px-2 align-middle text-white"
+        style={{ backgroundColor: "#ee7a1b" }}
+      >
         <div>
+          {/* <NavLink to="/">
+            <span>Home</span>
+          </NavLink> */}
+        </div>
+        <div className="text-4xl font-bold justify-center">CFL CLT Connect</div>
+        <div className="flex flex-row justify-between align-middle pr-4 py-3 text-lg">
           <NavLink to="/">
             <span>Home</span>
           </NavLink>
-        </div>
-        <div className="text-4xl">CFL CLT Connect</div>
-        <div className="flex flex-row justify-between align-middle">
           <NavLink to="/data_table">
             <div className="mx-2">Grants</div>
           </NavLink>
           <NavLink to="/subscribe">
-            <div>Subscribe for Alerts</div>
+            <div>Subscribe</div>
           </NavLink>
         </div>
       </div>
