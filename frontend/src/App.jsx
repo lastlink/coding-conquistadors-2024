d6736c3f149b7c27bbe32892a49b5e1ef3f38396
David Kerr <kerrky@gmail.com>
2024-11-02 14:20:56 -0400

    Updated emails and tables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,16 +9,42 @@ import "./index.css";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import SubscribePage from "./pages/SubscribePage";
+import DataPage from "./pages/DataPage";
+import { useEffect, useState } from "react";
 
 function App() {
+  // API fetch requests go here:
+  const [funding, setFunding] = useState([]);
+
+  const apiURL = "";
+
+  useEffect(() => {
+    const fetchFunding = async () => {
+      try {
+        const res = await fetch(`${apiURL}/`);
+        const data = await res.json();
+        setFunding(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
+    };
+    fetchFunding();
+  }, []);
+
+  // API POST requests go here:
+
+  // Routes go here:
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/subscribe" element={<SubscribePage />} />
+        <Route path="/data_table" element={<DataPage fundings={funding} />} />
       </Route>
     )
   );
+  // Routes end here
   return <RouterProvider router={router} />;
 }
 
